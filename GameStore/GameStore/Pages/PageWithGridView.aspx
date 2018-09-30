ada00a9d1d3573c41e41380916558a95227a4fa0
dgaliulin <dgaliulin@at-consulting.ru>
2018-09-30 21:41:44 +0300

    SqlDataSource

---
+++ GameStore/GameStore/Pages/PageWithGridView.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PageWithGridView.aspx.cs" Inherits="GameStore.PageWithGridView" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>GridView</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:GridView ID="grid" runat="server" AutoGenerateColumns="true" />
+    </form>
+</body>
+</html>
---
+++ GameStore/GameStore/Pages/PageWithGridView.aspx.cs
@@ -0,0 +1,39 @@
+﻿using GameStore.Models;
+using GameStore.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GameStore
+{
+    public partial class PageWithGridView : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["gridViewDBConnection"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand command = new SqlCommand("SELECT GameID, Name, Description, Category, Price FROM Games", connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                grid.DataSource = reader;
+                grid.DataBind();
+                reader.Close();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
